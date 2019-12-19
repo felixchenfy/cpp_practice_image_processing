72724097e3d9d35eb813fc04aac56d79419d6c7d
feiyu <felixchenfy@gmail.com>
2019-12-19 00:06:12 -0600

    Updated RANSAC

--- include/my_ransac/ransac.h
+++ include/my_ransac/ransac.h
@@ -11,24 +11,8 @@ namespace ransac
 {
 typedef std::vector<unsigned int> Indices;
 
-unsigned int countDataSmallerThan(const std::vector<double> &errors, const double thresh)
-{
-    unsigned int cnt = 0;
-    for (const double error : errors)
-        if (error < thresh)
-            cnt += 1;
-    return cnt;
-}
-
-Indices getIndicesOfDataSmallerThan(const std::vector<double> &errors, const double thresh)
-{
-    Indices indices;
-    int P = errors.size();
-    for (unsigned int i = 0; i < P; i++)
-        if (errors[i] < thresh)
-            indices.push_back(i);
-    return indices;
-}
+unsigned int countDataSmallerThan(const std::vector<double> &errors, const double thresh);
+Indices getIndicesOfDataSmallerThan(const std::vector<double> &errors, const double thresh);
 
 template <typename Datum, typename Param>
 Indices ransac(
@@ -87,7 +71,7 @@ Indices ransac(
 
                 if (is_print)
                 {
-                    std::cout << "    This is a better model!!! "<< std::endl;
+                    std::cout << "    This is a better model!!! " << std::endl;
                 }
             }
         }
--- src/my_ransac/ransac.cpp
+++ src/my_ransac/ransac.cpp
@@ -2,4 +2,24 @@
 
 namespace ransac
 {
+
+unsigned int countDataSmallerThan(const std::vector<double> &errors, const double thresh)
+{
+    unsigned int cnt = 0;
+    for (const double error : errors)
+        if (error < thresh)
+            cnt += 1;
+    return cnt;
 }
+
+Indices getIndicesOfDataSmallerThan(const std::vector<double> &errors, const double thresh)
+{
+    Indices indices;
+    int P = errors.size();
+    for (unsigned int i = 0; i < P; i++)
+        if (errors[i] < thresh)
+            indices.push_back(i);
+    return indices;
+}
+
+} // namespace ransac
